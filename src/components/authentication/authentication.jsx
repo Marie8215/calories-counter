e26a5f9472798817b0bb8147d605777b45781631
Permiakova Maria <f.marie1998@gmail.com>
2025-06-01 21:23:42 +0500

    Стилизация страницы входа

--- src/components/authentication/authentication.jsx
+++ src/components/authentication/authentication.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { useNavigate } from "react-router";
 import styles from "./authentication.module.css";
 import { ServerAdress } from "../../apiConstant";
+import { Button, Checkbox, Form, Input, Typography } from "antd";
+import logo from "../../images/logo.jpg"
 
 export const Authentication = () => {
   const [username, setUsername] = useState("");
@@ -32,27 +34,55 @@ export const Authentication = () => {
 
   return (
     <div className={styles.authContainer}>
-      <h2>Аутентификация</h2>
-      <div>
-        <div>
-          <label>Логин:</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Пароль:</label>
-          <input
-            type="text"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button onClick={tryLogin}>Войти</button>
-        {errorMessage && <p>{errorMessage}</p>}
+      <div className={styles.authFormContainer}>
+      <div className={styles.authLogo}>
+        <img src={logo} alt="CalorieTracker" className={styles.logoImage} />
+        <Typography.Title level={3} style={{ marginBottom: 0 }}>
+          Калькулятор калорий
+        </Typography.Title>
       </div>
+        
+        <Form
+          name="auth-form"
+          initialValues={{ remember: true }}
+        >
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: 'Введите ваш email!' }]}
+          >
+            <Input placeholder="Email" size="large" />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Введите ваш пароль!' }]}
+          >
+            <Input.Password placeholder="Пароль" size="large" />
+          </Form.Item>
+
+          <Form.Item>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Form.Item name="remember" valuePropName="checked">
+              <Checkbox>Запомнить меня</Checkbox>
+            </Form.Item>
+            <a>
+              Забыли пароль?
+            </a>
+            </div>
+          </Form.Item>
+          {errorMessage && (
+            <div style={{ color: 'red', marginBottom: 16, textAlign: 'center' }}>
+              {errorMessage}
+            </div>
+          )}
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block >
+              Войти
+            </Button>
+          </Form.Item>
+        </Form>
+        </div>
     </div>
   );
 };
